8db3f548be6bc34fcec9cce3887a6bcfc5203335
aaronms1 <aaronms1@protonmail.com>
2022-10-23 12:47:00 -0400

    stubbedExceptions

---
+++ src/com/amazon/ata/music/playlist/service/exceptions/InvalidAttributeChangeException.java
@@ -0,0 +1,21 @@
+package com.amazon.ata.music.playlist.service.exceptions;
+
+public class InvalidAttributeChangeException extends RuntimeException {
+    private static final long serialVersionUID = 142644508873652605L;
+    public InvalidAttributeChangeException() {
+        super();
+    }
+    
+    public InvalidAttributeChangeException(String message) {
+        super(message);
+    }
+    
+    public InvalidAttributeChangeException(Throwable cause) {
+        super(cause);
+    }
+    
+    public InvalidAttributeChangeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
